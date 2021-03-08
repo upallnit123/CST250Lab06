c733471bde80388d2a19d9024787f3303ec66941
upallnit123 <78699219+upallnit123@users.noreply.github.com>
2021-03-08 13:10:56 -0600

    Add files via upload

---
+++ CST250Lab06/Lab6/src/main/java/edu/ru/lab6/Rectangle.java
@@ -0,0 +1,29 @@
+package edu.ru.lab6;
+
+/**
+ *
+ * @author upall
+ */
+public class Rectangle {
+
+    int width;
+    int height;
+    
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+    public void draw() {
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print("*");
+            }
+        System.out.println("");
+        
+            
+        }
+    }
+    
+}
